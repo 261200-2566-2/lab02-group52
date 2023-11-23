544d1b1bf68d77a3c42b1dea00b45e54750ad5fa
Kawin <kawin_chusin@cmu.ac.th>
2023-11-23 17:25:20 +0700

    implement AirPurifier class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        // Create an instance of the AirPurifier class
+        AirPurifier purifier = new AirPurifier("Model123", "SN456");
+        AirPurifier purifier1 = new AirPurifier("Model1234", "SN4567");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Call the OnOff method to toggle the power status
+        purifier.Onoff();
+        purifier.Onoff();
+        purifier.Onoff();
+        purifier.speedwind();
+        purifier.speedwind();
+        purifier.speedwind();
+        purifier.speedwind();
+        purifier.settime();
+        purifier.settime();
+        purifier.settime();
+        purifier.settime();
+        purifier.settime();
+        purifier.settime();
+        purifier.settime();
+        purifier.mode();
+        purifier.mode();
+        purifier.mode();
+        purifier.Onoff();
+        AirPurifier.checkstock();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
